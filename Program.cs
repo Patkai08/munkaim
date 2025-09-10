3ff11549a506ab2819cfac03b6f3cf6390a8990f
Patkai08 <145003788+Patkai08@users.noreply.github.com>
2025-09-10 18:38:56 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,44 +1,45 @@
-﻿namespace ConsoleApp1
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._10
 {
-
-    class Auto
-    {
-        public string marka;
-        public bool elektromos;
-        public int atjok;
-        public string szin;
-        public int ar;
-        public int km;
-        public int evjarat;
-
-        public Auto(string marka, bool elektromos, int ajtok, string szin, int ar, int km, int evjarat)
-        {
-            this.marka = marka;
-            this.elektromos = elektromos;
-            this.atjok = ajtok;
-            this.szin = szin;
-            this.ar = ar;
-            this.km = km;
-            this.evjarat = evjarat;
-        }
-    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Auto auto1 = new Auto("Ford", false, 5, "barna", 2800000, 170000, 2015);
-            Auto auto2 = new Auto("Volvo", false, 5, "bézs", 3000000, 400000, 2008);
-            Auto auto3 = new Auto("Skoda", true, 5, "fehér", 5000000, 100000, 2022);
-
-            Auto[] autok = new Auto[5];
-            autok[0] = auto1;
-            autok[1] = auto2;
-            autok[2] = auto3;
-
-            for (int i = 0; i < autok.Length; i++)
+            //double, int, string, char, bool
+            int[,] t2 = new int[5, 5];
+            int[][] asd = new int[5][];
+            asd[0] = new int[5];
+            Console.WriteLine(asd[0][0]);
+            int szam = 5;
+            if(szam > 0 && szam < 0)
             {
-                Console.WriteLine(  );
+
             }
+            if(szam % 2 == 0)
+            {
+
+            }
+            int m = 160;
+            int n = 35;
+
+            void LNKO(int m, ref int n)
+            {
+                int r = m % n;
+                while (r != 0)
+                {
+                    m = n;
+                    n = r;
+                    r = m % n;
+                }
+            }
+
+            LNKO(m, ref n);
+            Console.WriteLine("LNKO: " + n);
         }
     }
 }
